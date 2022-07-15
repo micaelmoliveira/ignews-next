a5dbe7d3e32d1a4cc3f1a2ab4d342e00e10fad39
Micael Oliveira <micaelmoura.oliveira@gmail.com>
2022-07-15 19:46:31 -0300

    Header

---
+++ src/components/Header/Index.tsx
@@ -0,0 +1,21 @@
+import Image from 'next/image';
+import logoSvg from '../../../public/images/logo.svg';
+import { SignInButton } from '../SignInButton/index';
+
+import styles from './styles.module.scss';
+
+export function Header(){
+    return(
+        <header className={styles.headerContainer}>
+            <div className={styles.headerContent}>
+                <Image src={logoSvg} alt=" ignews seu blog de tecnologia" />
+                <nav>
+                    <a className={styles.active}>Home</a>
+                    <a>Posts</a>
+                </nav>
+
+                <SignInButton />
+            </div>
+        </header>
+    )
+}
